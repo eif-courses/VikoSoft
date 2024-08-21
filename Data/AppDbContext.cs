0a47417f0593f8be4b5d4008a1b713ab40973599
Marius <m.gzegozevskis@gmail.com>
2024-08-22 02:46:30 +0300

    v0.0.5-entities

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,7 +1,6 @@
-﻿
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using Tarifikacija.Entities;
+
 using VikoSoft.Data.Entities;
 
 namespace VikoSoft.Data;
@@ -9,28 +8,32 @@ namespace VikoSoft.Data;
 public class AppDbContext(DbContextOptions<AppDbContext> options)
     : IdentityDbContext<ApplicationUser>(options)
 {
-    
     // Magic string.
     public static readonly string RowVersion = nameof(RowVersion);
 
     // Magic strings.
     public static readonly string ApplicationDb = nameof(ApplicationDb).ToLower();
 
-    
-    
-    
+
     // public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     // {
     //     Debug.WriteLine($"{ContextId} context created.");
     // }
 
-    
-    public DbSet<Report> Reports { get; init; }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<SubjectType> SubjectTypes { get; set; }
+    public DbSet<Faculty> Faculties { get; set; }
+    public DbSet<Department> Departments { get; set; }
+    public DbSet<StudyPlan> StudyPlans { get; set; }
+    public DbSet<StudyProgram> StudyPrograms { get; set; }
+    public DbSet<StudyForm> StudyForms { get; set; }
+    public DbSet<StudentGroup> StudentGroups { get; set; }
     public DbSet<Subject> Subjects { get; set; }
     public DbSet<ContactHours> ContactHours { get; set; }
     public DbSet<NonContactHours> NonContactHours { get; set; }
-    public DbSet<SubjectType> SubjectTypes { get; set; }
-    
+    public DbSet<ContactHoursDetails> ContactHoursDetails { get; set; }
+    public DbSet<NonContactHoursDetails> NonContactHoursDetails { get; set; }
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         // This property isn't on the C# class,
@@ -38,28 +41,76 @@ public class AppDbContext(DbContextOptions<AppDbContext> options)
         modelBuilder.Entity<Report>()
             .Property<byte[]>(RowVersion)
             .IsRowVersion();
-        
-        modelBuilder.Entity<BaseEntity>()
-            .Property(e => e.Id)
-            .HasConversion(
-                v => v.ToString(),
-                v => Ulid.Parse(v.ToString()));
-        
+
+        modelBuilder.Entity<BaseEntity>().HasKey(be => be.Id);
+        modelBuilder.Entity<BaseEntity>().Property(be => be.Id).HasConversion(
+            id => id.ToString(),
+            id => Ulid.Parse(id.ToString()));
+
+        modelBuilder.Entity<Category>().ToTable("Categories");
+        modelBuilder.Entity<SubjectType>().ToTable("SubjectTypes");
+        modelBuilder.Entity<Faculty>().ToTable("Faculties");
+        modelBuilder.Entity<Department>().ToTable("Departments");
+        modelBuilder.Entity<StudyPlan>().ToTable("StudyPlans");
+        modelBuilder.Entity<StudyProgram>().ToTable("StudyPrograms");
+        modelBuilder.Entity<StudyForm>().ToTable("StudyForms");
+        modelBuilder.Entity<StudentGroup>().ToTable("StudentGroups");
+        modelBuilder.Entity<Subject>().ToTable("Subjects");
+        modelBuilder.Entity<ContactHours>().ToTable("ContactHours");
+        modelBuilder.Entity<NonContactHours>().ToTable("NonContactHours");
+        modelBuilder.Entity<ContactHoursDetails>().ToTable("ContactHoursDetails");
+        modelBuilder.Entity<NonContactHoursDetails>().ToTable("NonContactHoursDetails");
+
         modelBuilder.Entity<Category>()
             .HasMany(c => c.SubjectTypes)
             .WithOne(st => st.Category)
-            .HasForeignKey(st => st.CategoryId)
-            .IsRequired();
+            .HasForeignKey(st => st.Id);
 
-        // Configure SubjectType -> Subject relationship
         modelBuilder.Entity<SubjectType>()
             .HasMany(st => st.Subjects)
-            .WithOne(s => s.Type)
-            .HasForeignKey(s => s.TypeId)
-            .IsRequired();
-        
-        
-        
+            .WithOne(s => s.SubjectType)
+            .HasForeignKey(s => s.Id);
+
+        modelBuilder.Entity<Faculty>()
+            .HasMany(f => f.Departments)
+            .WithOne(d => d.Faculty)
+            .HasForeignKey(d => d.Id);
+
+        modelBuilder.Entity<Department>()
+            .HasMany(d => d.StudyPlans)
+            .WithOne(sp => sp.Department)
+            .HasForeignKey(sp => sp.Id);
+
+        modelBuilder.Entity<Department>()
+            .HasMany(d => d.StudentGroups)
+            .WithOne(sg => sg.Department)
+            .HasForeignKey(sg => sg.Id);
+
+        modelBuilder.Entity<StudentGroup>()
+            .HasOne(sg => sg.Faculty)
+            .WithMany(f => f.StudentGroups)
+            .HasForeignKey(sg => sg.Id);
+
+        modelBuilder.Entity<Subject>()
+            .HasOne(s => s.ContactHours)
+            .WithOne(ch => ch.Subject)
+            .HasForeignKey<ContactHours>(ch => ch.Id);
+
+        modelBuilder.Entity<Subject>()
+            .HasOne(s => s.NonContactHours)
+            .WithOne(nch => nch.Subject)
+            .HasForeignKey<NonContactHours>(nch => nch.Id);
+
+        modelBuilder.Entity<ContactHours>()
+            .HasOne(ch => ch.ContactHoursDetails)
+            .WithOne(chd => chd.ContactHours)
+            .HasForeignKey<ContactHoursDetails>(chd => chd.Id);
+
+        modelBuilder.Entity<NonContactHours>()
+            .HasOne(nch => nch.NonContactHoursDetails)
+            .WithOne(nchd => nchd.NonContactHours)
+            .HasForeignKey<NonContactHoursDetails>(nchd => nchd.Id);
+
         base.OnModelCreating(modelBuilder);
     }
 }
--- Data/Entities/Subject.cs
+++ Data/Entities/Subject.cs
@@ -1,64 +1,38 @@
-﻿using Tarifikacija.Entities;
-
+﻿
 namespace VikoSoft.Data.Entities;
+
+
+public abstract class BaseEntity
+{
+    public Ulid Id { get; set; } = Ulid.NewUlid();
+}
 
 public class Category : BaseEntity
 {
-    public string Title { get; set; } // The name of the category, e.g., "General College Study Subjects"
-
+    public string Title { get; set; }
     public ICollection<SubjectType> SubjectTypes { get; set; } = new List<SubjectType>();
 }
 
 public class SubjectType : BaseEntity
 {
     public string Name { get; set; }
-    public Category Category { get; set; }
     public ICollection<Subject> Subjects { get; set; } = new List<Subject>();
-    public int CategoryId { get; set; }
+    public Category Category { get; set; }
 }
 
 public class Faculty : BaseEntity
 {
     public string Name { get; set; }
+    public ICollection<Department> Departments { get; set; } = new List<Department>();
+    public ICollection<StudentGroup> StudentGroups { get; set; } = new List<StudentGroup>();
 }
 
 public class Department : BaseEntity
 {
     public string Name { get; set; }
-}
-
-public class ContactHours : BaseEntity
-{
-    public int LectureHours { get; set; }
-    public int PracticeHours { get; set; }
-    public int? RemoteLectureHours { get; set; }
-    public int? RemotePracticeHours { get; set; }
-    public int SelfStudyHours { get; set; }
-    public string Notes { get; set; }
-    public ContactHoursDetails ContactHoursDetails { get; set; }
-}
-
-public class NonContactHours : BaseEntity
-{
-    public NonContactHoursDetails NonContactHoursDetails { get; set; }
-}
-
-public class ContactHoursDetails : BaseEntity
-{
-    public int SubGroupsCount { get; set; }
-    public int LecturesCount { get; set; }
-    public int FinalProjectExamCount { get; set; }
-    public int OtherCount { get; set; }
-    public int ConsultationCount { get; set; }
-    public int TotalContactHours { get; set; }
-}
-
-public class NonContactHoursDetails : BaseEntity
-{
-    public int GradingNumberCount{ get; set; }
-    public int GradingHours { get; set; }
-    public int OtherCount{ get; set; }
-    // TODO ADD MORE PROPS
+    public Faculty Faculty { get; set; }
+    public ICollection<StudyPlan> StudyPlans { get; set; } = new List<StudyPlan>();
+    public ICollection<StudentGroup> StudentGroups { get; set; } = new List<StudentGroup>();
 }
 
 public class StudyPlan : BaseEntity
@@ -95,10 +69,44 @@ public class Subject : BaseEntity
     public string Title { get; set; }
     public int Credits { get; set; }
     public int Semester { get; set; }
-    public SubjectType Type { get; set; }
-
     public ContactHours ContactHours { get; set; }
     public NonContactHours NonContactHours { get; set; }
+    public SubjectType SubjectType { get; set; }
+}
 
-    public int TypeId { get; set; }
+public class ContactHours : BaseEntity
+{
+    public int LectureHours { get; set; }
+    public int PracticeHours { get; set; }
+    public int? RemoteLectureHours { get; set; }
+    public int? RemotePracticeHours { get; set; }
+    public int SelfStudyHours { get; set; }
+    public string Notes { get; set; }
+    public ContactHoursDetails ContactHoursDetails { get; set; }
+    public Subject Subject { get; set; }
+}
+
+public class NonContactHours : BaseEntity
+{
+    public NonContactHoursDetails NonContactHoursDetails { get; set; }
+    public Subject Subject { get; set; }
+}
+
+public class ContactHoursDetails : BaseEntity
+{
+    public int SubGroupsCount { get; set; }
+    public int LecturesCount { get; set; }
+    public int FinalProjectExamCount { get; set; }
+    public int OtherCount { get; set; }
+    public int ConsultationCount { get; set; }
+    public int TotalContactHours { get; set; }
+    public ContactHours ContactHours { get; set; }
+}
+
+public class NonContactHoursDetails : BaseEntity
+{
+    public int GradingNumberCount { get; set; }
+    public int GradingHours { get; set; }
+    public int OtherCount { get; set; }
+    public NonContactHours NonContactHours { get; set; }
 }
